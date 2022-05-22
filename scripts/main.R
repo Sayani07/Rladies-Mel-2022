8e3aab388c8b82b4b912cc53e9da1e61deca8a73
Sayani07 <Sayani.Gupta@monash.edu>
2022-05-22 21:42:10 +1000

    same geom different stat and vice versa

--- scripts/main.R
+++ scripts/main.R
@@ -27,3 +27,15 @@ class(geom_boxplot()$stat)
 class(geom_hdr_boxplot()$geom)
 class(geom_hdr_boxplot()$stat)
 
+
+b <- ggplot(mtcars, aes(wt, mpg)) +
+  geom_point()
+
+df <- data.frame(x1 = 2.62, x2 = 3.57, y1 = 21.0, y2 = 15.0)
+
+b +
+  geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2, colour = "segment"),
+               data = df)
+
+b +
+  geom_spoke(aes(angle = 45), radius = 0.5)
